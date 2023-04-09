e22f6df5cb125104999d1edd21376e35bc5767a5
Rogério-Mint <rogerio.barbosa.pf@gmail.com>
2023-04-09 08:09:17 -0300

    criando pagina login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
@@ -12,3 +12,17 @@ def index():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+
+    return 'Usuário: {} e senha {}'.format(usuario, senha)
